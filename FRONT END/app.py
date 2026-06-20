fbc3d17e60aadfcafc837cd2380a8527ed5b67c5
unknown <achyuthareddyborampeta@gmail.com>
2026-06-20 14:46:07 +0530

    prepare for deployment

--- FRONT END/app.py
+++ FRONT END/app.py
@@ -139,5 +139,8 @@ def prediction():
 def graph():
     return render_template('graph.html')
 
-if __name__ == '__main__':
-    app.run(debug = True)
+# if __name__ == '__main__':
+#     app.run(debug = True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
